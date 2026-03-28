b599deb3e203541b8a598def78b6b1f78ba32e2a
Ruvarashe Shoko <ruvarashe@Kali.lan>
2026-03-28 16:42:11 +0200

    Add clean requirements.txt for Render deployment

--- expense_management/settings.py
+++ expense_management/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME", "gzu2"),
         "USER": os.environ.get("DB_USER", "root"),
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT", "3306"),
     }
 }
